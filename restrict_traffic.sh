e4e829ce5745b779c245fa6701c20ebdc1953407
Michael Mendy <montana@linux.com>
2025-02-14 13:55:06 -0800

    restrict traffic from unknown applications to prevent evasion techniques

---
+++ restrict_traffic.sh
@@ -0,0 +1,8 @@
+set rulebase security rules "Block-Unknown" action deny
+set rulebase security rules "Block-Unknown" application any
+set rulebase security rules "Block-Unknown" category any
+set rulebase security rules "Block-Unknown" from any
+set rulebase security rules "Block-Unknown" to any
+set rulebase security rules "Block-Unknown" source any
+set rulebase security rules "Block-Unknown" destination any
+commit
